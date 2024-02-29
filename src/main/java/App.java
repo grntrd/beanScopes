27e9e0c5f5da09aa39f87f9ce28f36a41fbc7ecd
pepega <endhold2@gmail.com>
2024-02-29 15:23:16 +0300

    Spring MVC bean scopes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,14 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld beanHw2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.equals(beanHw2));
         System.out.println(bean.getMessage());
+
+        Cat beanCat = (Cat) applicationContext.getBean("cat");
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat.equals(beanCat2));
+        System.out.println(beanCat.getName());
     }
 }
